420e840927f41a4fce7f438a4ae75d4ca53125e1
apenasLotus <git624886@gmail.com>
2022-07-13 01:11:56 -0300

    feat:inicio do controlador de home

---
+++ app/Controller/Pages/Home.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace app\Controller\Pages;
+
+class Home
+{
+    /**
+     * Método responsável por retornar o conteúdo view da Home
+     * @return string (Conteúdo HTML a ser impresso)
+     */
+    public static function getHome()
+    {
+        return 'Salve';
+    }
+}
