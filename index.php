06bb7167ccc448b17971b124c0f0596aa4e62496
Gabriel Vitor <git624886@gmail.com>
2022-07-13 10:01:05 -0300

    feat:instanciada classe de response

--- index.php
+++ index.php
@@ -3,5 +3,12 @@
 require __DIR__."/vendor/autoload.php";
 
 use app\Controller\Pages\Home;
+use app\Http\Request;
+use app\Http\Response;
+
+$obResponse = new Response(404, 'Olá mundo');
+$obResponse->sendResponse();
+
+exit;
 
 echo Home::getHome();
