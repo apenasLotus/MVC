876aaa8caa40f35fb1fdaa5acfb9385d476ad573
Gabriel Vitor <git624886@gmail.com>
2022-07-13 11:54:13 -0300

    feat:adicionado metodo addRoute na classe de Router

--- app/Http/Router.php
+++ app/Http/Router.php
@@ -2,6 +2,8 @@
 
 namespace app\Http;
 
+use Closure;
+
 class Router
 {
     /**
@@ -53,5 +55,45 @@ class Router
 
         //Define o prefixo
         $this->prefix = $parseUrl['path'] ?? '';
+    }
+
+    /**
+     * Método responsável por adicionar uma rota na classe
+     * @param string $method
+     * @param string $route
+     * @param array $params
+     */
+    public function addRoute($method, $route, $params = [])
+    {
+        //Validação dos parâmetros
+        foreach ($params as $key => $value) {
+
+            /**
+             * Trocando a posição numérica no array, para um 'controller'
+             */
+
+            //Se $value for uma instancia de Closure
+            if ($value instanceof Closure) {
+                $params['controller'] = $value;
+                unset($params[$key]);
+                continue;
+            }
+        }
+
+        //Padrão de validação da URL
+        $patternRoute = '/^'.str_replace('/', '\/', $route). '$/';
+
+        //Adiciona a rota dentro da classe
+        $this->routes[$patternRoute][$method] = $params;
+    }
+
+    /**
+     * Método responsável por definir uma rota de GET
+     * @param string $route
+     * @param array $params
+     */
+    public function get($route, $params = [])
+    {
+        return $this->addRoute('GET', $route, $params);
     }
 }
