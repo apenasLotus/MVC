3f9a19ac4d43f55c47589ae695e8ceb8479f08c4
Gabriel Vitor <git624886@gmail.com>
2022-07-13 10:40:14 -0300

    feat:adicionada classe de rotas

---
+++ app/Http/Router.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace app\Http;
+
+class Router
+{
+    /**
+     * URL completa do projeto (raiz)
+     * @var string
+     */
+    private $url = '';
+
+    /**
+     * Prefixo de todas as rotas
+     * @var string
+     */
+    private $prefix = '';
+
+    /**
+     * Índice de rotas
+     * @var array
+     */
+    private $routes = [];
+
+    /**
+     * Instancia de Request
+     * @var Request
+     */
+    private $request;
+
+    /**
+     * Método responsável por iniciar a classe
+     * @param string $url
+     */
+    public function __construct($url)
+    {
+        $this->request = new Request();
+        $this->url     = $url;
+        $this->setPrefix();
+    }
+
+    /**
+     * Método responsável por definir o prefixo das rotas
+     */
+    private function setPrefix()
+    {
+        /**
+         * Informações da URL atual
+         * Prefix = ['path']
+         * Caso não exista, não tem oque remover
+         */
+        $parseUrl = parse_url($this->url);
+
+        //Define o prefixo
+        $this->prefix = $parseUrl['path'] ?? '';
+    }
+}
