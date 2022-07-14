d7baf30f1cefba54c8180ee5ea72de4f1156d12d
Gabriel Vitor <git624886@gmail.com>
2022-07-14 12:16:13 -0300

    feat:adicionada rota com variaveis dinamicas

--- routes/Pages.php
+++ routes/Pages.php
@@ -5,15 +5,22 @@ use App\Controller\Pages;
 
 
 //Rota HOME
-$obRouter->get('/',[
-    function(){
-        return new Response(200,Pages\Home::getHome());
+$obRouter->get('/', [
+    function () {
+        return new Response(200, Pages\Home::getHome());
     }
 ]);
 
 //Rota SOBRE
-$obRouter->get('/sobre',[
-    function(){
-        return new Response(200,Pages\About::getHome());
+$obRouter->get('/sobre', [
+    function () {
+        return new Response(200, Pages\About::getHome());
+    }
+]);
+
+//Rota DINÂMICA
+$obRouter->get('/sobre/{idPagina}', [
+    function ($idPagina) {
+        return new Response(200, $idPagina);
     }
 ]);
