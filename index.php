859c1d988791d77b96e6728d5773839b6feaeace
Gabriel Vitor <git624886@gmail.com>
2022-07-14 10:45:16 -0300

    feat:define o valor padrao das variaveis pelo metodo init

--- index.php
+++ index.php
@@ -1,21 +1,30 @@
 <?php
 
-require __DIR__."/vendor/autoload.php";
+require __DIR__ . "/vendor/autoload.php";
 
 use app\Http\Router;
+use App\Utils\View;
 
 /**
- * ! Define a const da pagina, e já usa a mesma
+ * ! Define a const da pagina, e inicia o ROUTER
  */
 define('URL', 'http://127.0.0.1:8080');
 $obRouter = new Router(URL);
 
 /**
+ * ! Define o valor padrão das variáveis
+ */
+View::init([
+    'URL' => URL
+]);
+
+/**
  * ! Inclui as rotas de páginas
  */
-include __DIR__.'/routes/Pages.php';
+include __DIR__ . '/routes/Pages.php';
 
 /**
  * ! Imprime o response da rota
  */
-$obRouter->run()->sendResponse();
+$obRouter->run()
+    ->sendResponse();
