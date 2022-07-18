51d16e13c4bc6afeac8b5f0efac5d1a8e1487223
Gabriel Vitor <git624886@gmail.com>
2022-07-18 07:58:41 -0300

    feat:adiciona conexao com banco de dados

--- includes/app.php
+++ includes/app.php
@@ -4,11 +4,23 @@ require __DIR__ . "/../vendor/autoload.php";
 
 use App\Utils\View;
 use WilliamCosta\DotEnv\Environment;
+use WilliamCosta\DatabaseManager\Database;
 
 /**
  * ! Carrega variáveis de ambiente
  */
-Environment::load(__DIR__.'/../');
+Environment::load(__DIR__ . '/../');
+
+/**
+ * ! Define as configurações de Banco de Dados
+ */
+Database::config(
+    getenv('DB_HOST'),
+    getenv('DB_NAME'),
+    getenv('DB_USER'),
+    getenv('DB_PASS'),
+    getenv('DB_PORT')
+);
 
 /**
  * ! Define a constante de URL
