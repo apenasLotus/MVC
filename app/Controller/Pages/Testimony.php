b594a2142104ed5296850e9f9083d3b2f786d530
Gabriel Vitor <git624886@gmail.com>
2022-07-19 11:53:20 -0300

    feat:criada classe getTestimonyItens porem quebrou

--- app/Controller/Pages/Testimony.php
+++ app/Controller/Pages/Testimony.php
@@ -4,10 +4,37 @@ namespace App\Controller\Pages;
 
 use App\Utils\View;
 use App\Model\Entity\Testimony as EntityTestimony;
+use LDAP\Result;
 
 //Classe com mesmo nome do arquivo.
 class Testimony extends Page
 {
+
+    /**
+     * Método responsável por obter a renderização dos itens de depoimentos
+     * @return string
+     */
+    private static function getTestimonyItens()
+    {
+        //Depoimentos
+        $itens = '';
+
+        //Resultados da página
+        $results = EntityTestimony::getTestimonies(null, 'id DESC');
+
+        //Renderiza o item
+        while ($obTestimony = $results->fetchObject(EntityTestimony::class)) {
+            $itens .= View::render('Pages/Testimonies/Item', [
+                'nome' => $obTestimony->nome,
+                'mensagem' => $obTestimony->mensagem,
+                'data' => date('d/m/Y H:i:s', strtotime($obTestimony->data))
+            ]);
+        }
+
+        //Retorna os depoimentos
+        return $itens;
+    }
+
     /**
      * Método responsável por retornar o conteúdo view de depoimentos
      * @return string (Conteúdo HTML a ser impresso)
@@ -15,7 +42,9 @@ class Testimony extends Page
     public static function getTestimonies()
     {
         //Recebe a view de depoimentos
-        $content = View::render('Pages/Testimonies', []);
+        $content = View::render('Pages/Testimonies', [
+            'itens' => self::getTestimonyItens()
+        ]);
 
         //Retorna a view da página
         return parent::getPage('DEPOIMENTOS - MVC', $content);
@@ -37,6 +66,6 @@ class Testimony extends Page
         $obTestimony->mensagem = $postVars['mensagem'];
         $obTestimony->cadastrar();
 
-        return self::getTestimonies();  
+        return self::getTestimonies();
     }
 }
